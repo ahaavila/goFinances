2a2bfad4a3c48f6c2f519a8b0a2fed85235de734
Augusto <ahaavila@gmail.com>
2020-05-06 19:09:39 -0300

    Adicionado todas as funcionalidades

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
+// Formatação dos valores de income e outcome
+
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
 export default formatValue;
